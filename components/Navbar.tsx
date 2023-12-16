3ea404d5a9199bc31df9e05c687bc45858cbca2e
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-12-16 11:38:30 +0200

    Create contact-me page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,15 @@
 import { Socials } from "@/constans";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
-        <div className="relative">
-          <Image
-            src="/horseLogo.jpg"
-            alt="logo"
-            width={40}
-            height={40}
-            className="w-full h-full object-contain rounded-full"
-          />
-        </div>
-        <h1 className="text-white text-[25px] font-semibold">Portfolio</h1>
+        <Link href="/">
+          <h1 className="text-white text-[25px] font-semibold">Portfolio</h1>
+        </Link>
       </div>
 
       <div className="flex flex-row gap-5 mb-2">
